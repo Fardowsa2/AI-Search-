f4eb491a541bfb0633283f74d048b58886b9339e
Fardowsa2 <110959975+Fardowsa2@users.noreply.github.com>
2025-11-26 15:14:00 -0600

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# src/__init__.py
-"""
-AI Search Algorithms Lab
-Implementation of DFS, BFS, IDDFS, Greedy Best-First, and A* search algorithms
-with visualization and benchmarking capabilities.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Fardowsa Muhumed"
